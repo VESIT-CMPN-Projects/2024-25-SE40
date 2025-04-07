8bfc10bff2e6f39a42108c2fc1ab5f208f46c885
Preetika Khilnaney <2023.preetika.khilnaney@ves.ac.in>
2025-04-07 16:05:44 +0530

    Add files via upload

---
+++ frontend/src/Components/heading/sectionheading.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './sectionheading.css'; 
+
+const SectionHeading = () => {
+  return (
+    <div className="section-heading">
+      <h1>A Collection That Defines Us</h1>
+    </div>
+  );
+};
+
+export default SectionHeading;
